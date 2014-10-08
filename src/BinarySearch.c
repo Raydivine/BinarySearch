11d80fab9bb6f18d51380e71f18f1f470a01dd84
raydivine <raydivine00@gmail.com>
2014-10-09 04:14:03 +0800

    done recusion searching

--- src/BinarySearch.c
+++ src/BinarySearch.c
@@ -2,41 +2,16 @@
 #include <stdio.h>
 
 int binarySearch( int target, int array[], int startIndex, int endIndex ){
-
-	int midIndex, Ans;
-	
+	int midIndex;
 	midIndex= (startIndex + endIndex)/2;
-	printf("%d\n", array[midIndex]);
 	
 	if( target == array[midIndex] )
 		return array[midIndex];
-	
-	if( target < array[midIndex] ){	
-		Ans = (startIndex + midIndex)/2;
-		printf("%d\n", array[Ans]);
-		
-		if( array[Ans] == target)
-			return array[Ans];
-		// else if( array[Ans+1] == target)
-			// return array[Ans+1];
-		// else if( array[Ans-1] == target)
-			// return array[Ans-1];		
-	//	binarySearch( target, array, Ans ,  midIndex);
-	}
-	
-	if( target > array[midIndex] ){	
-		Ans = (endIndex + midIndex)/2;
-		printf("%d\n", array[Ans]);
-		
-		if( array[Ans] == target)
-			return array[Ans];
-		// else if( array[Ans+1] == target)
-			// return array[Ans+1];
-		// else if( array[Ans-1] == target)
-			// return array[Ans-1];
-	//	binarySearch( target, array, midIndex, Ans );
-	}
-	
-
-
+	else if( startIndex == endIndex)
+		return -1;
+	else if( target < array[midIndex] )
+		binarySearch(target, array, startIndex, midIndex - 1);
+	else 
+		binarySearch(target, array, midIndex + 1, endIndex);
 }
+
--- test/test_BinarySearch.c
+++ test/test_BinarySearch.c
@@ -10,28 +10,15 @@ void tearDown(void)
 {
 }
 
-void test_BinarySearch_for_search_3_inside_the_back_array(){
+void test_BinarySearch_for_same_Start_and_end_index_should_return_negative1(){
 	int ans;
-	int array[] = {0,1,2,3,4};
-	int target  = 3 ;
-	int startIndex = 1;
-	int endIndex = 5;
+	int array[] = {1,3,8, 9,235,333, 444};
+	int target  = 8 ;
+	int startIndex = 7;
+	int endIndex = 7;
 	
 	ans = binarySearch( target, array, startIndex , endIndex );	
-	TEST_ASSERT_EQUAL( 3, ans );
-	
-}
-
-void test_BinarySearch_for_search_3_inside_the_front_array(){
-	int ans;
-	int array[] = {1,3,9,235};
-	int target  = 3 ;
-	int startIndex = 1;
-	int endIndex = 4;
-	
-	ans = binarySearch( target, array, startIndex , endIndex );	
-	TEST_ASSERT_EQUAL( 3, ans );
-	
+	TEST_ASSERT_EQUAL( -1, ans );
 }
 
 void test_BinarySearch_for_search_8_inside_the_front_array(){
@@ -56,31 +43,24 @@ void test_BinarySearch_for_search_333_inside_the_back_array(){
 	TEST_ASSERT_EQUAL( 333 , ans );
 }
 
-void test_BinarySearch_for_search_2_inside_the_front_array(){
+void test_BinarySearch_for_search_2_inside_the_2ndfront_array(){
 	int ans;
 	int array[] = {1,2,3,4,5,6, 7,8,9,10};
 	int target  = 2 ;
 	int startIndex = 1;
 	int endIndex = 10;
 	
-  printf("start\n");
 	ans = binarySearch( target, array, startIndex , endIndex );	
-	printf("end");
-	TEST_ASSERT_EQUAL( 2, ans );
-	
+	TEST_ASSERT_EQUAL( 2, ans );	
 }
 
-void xtest_BinarySearch_for_search_5_inside_the_front_array(){
+void test_BinarySearch_for_search_9_inside_the_2ndback_array(){
 	int ans;
 	int array[] = {1,2,3, 4,5,6, 7,8,9,10};
-	int target  = 5 ;
+	int target  = 9;
 	int startIndex = 1;
 	int endIndex = 10;
 	
-	printf("start\n");
 	ans = binarySearch( target, array, startIndex , endIndex );	
-	printf("end");
-	
-	TEST_ASSERT_EQUAL( 5 , ans );
-	
+	TEST_ASSERT_EQUAL( 9 , ans );
 }
